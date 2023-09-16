d88432218655ffb0aafb39eef9d451df9d558e35
kiryamoge <139700463+kiryamoge@users.noreply.github.com>
2023-09-16 10:53:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ const link = location.href;
 //2.1. Вывести в консоль каждое из имен (содержимое каждого li).
 //2.2. Поменять имена в списке выше на числа от 0 по порядку (0, 1, 2 и т.д.)
 
-const list = document.getElementById('list');
-const items = list.getElementsByTagName('li');
-for (let i = 0; i < items.length; i++) {
-  items[i].textContent = i;
-  console.log(items[i].textContent);
+const list = document.querySelectorAll('#list li');
+console.log(list);
+for (let i = 0; i < list.length; i++) {
+  list[i].textContent = i;
+  console.log(list[i].textContent);
 }
 //Задание 3
 //Добавить к нескольким тегам на странице класс "forRemove". Далее написать JS код, который найдет в HTML все элементы с классом "forRemove" и удалит их.
@@ -43,15 +43,28 @@ forRemove.forEach(function(elem) {
  document.body.append(paragraph);
 
 
+//Задание 5
+//Написать функцию, которая принимает на вход 3 параметра: название тега, название цвета, содержимое. Функция должна сформировать необходимый тег, добавить необходимый стиль с цветом и внести содержимое. Вывести несколько таких сгенерированных тегов в консоль, затем отправить их на страницу.
+
+const createTag = (tagName, color, content) => {
+  const tag = document.createElement(tagName);
+  tag.style.color = color;
+  tag.textContent = content;
+  return tag;
+}
+const h3 = createTag('h3','green','text');
+const p = createTag('p','blue',' some text');
+document.body.append(createTag('h2','yellow','text'));
+
+
 //Задание 6
 //Вставить в страницу (в html документ) тег <select>. С помощью js (в цикле) добавить в этот select опции (option) под годы от 1960 по 2020.
 
 const select = document.getElementById('yearSelect');
 for (let year = 1960; year <= 2020; year++) {
   const option = document.createElement('option');
-  option.text = year;
-  option.value = year;
-  select.add(option);
+  option.textContent = year;
+  select.append(option);
 }
 
 
@@ -90,12 +103,60 @@ let linksArr = ['https://www.amazon.com/', 'https://www.youtube.com/', 'https://
 //При нажатии на ссылки адреса должны открываться в новой вкладке (атрибут target="_blank")
 //3) Добавить DIV из пункта 1 (со ссылками внутри ) в BODY
 
-const creatDiv = document.createElement('div');
-creatDiv.style.backgroundColor = 'blue';
-creatDiv.style.padding = '70px';
+const div = document.createElement('div');
+div.style.backgroundColor = 'blue';
+div.style.padding = '70px';
 
 linksArr.forEach(item => {
-  linksArr.href = item;
-  linksArr.setAttribute('target', '_blank');
+  const a = document.createElement('a');
+  a.textContent = item;
+  a.href = item;
+  div.append(a);
+})
+document.body.append(div);
+
+
+//Задание 9*
+//Есть массив объектов с полями name, age. Например:
+const users = [
+{name: 'Mark', age: 12},
+{name: 'Olga', age: 30},
+{name:'Tom', age: 25},
+{name:'Den', age: 43}
+]
+//Создать в html таблицу (table).
+//C помощью js заполнить таблицу информацией из массива, в одной колонке будут имена, во второй возраст. Имена должны быть красного цвета, age - синего.
+//ПОДСКАЗКА, Таблица состоит из строк (tr) и ячеек (td) внутри этих строк. Строк должно создаваться столько, сколько объектов внутри массива, и их количество может быть любым.
+
+const table = document.querySelector('table');
+users.forEach(item => {
+  const tr = document.createElement('tr');
+  const tdName = document.createElement('td');
+  const tdAge = document.createElement('td');
+  tdName.textContent = item.name;
+  tdAge.textContent = item.age;
+  tr.append(tdName);
+  tr.append(tdAge);
+  table.append(tr);
 })
 
+
+//Задание 10
+//Есть верстка: 
+//С помощью JavaScript:
+//1) Найти в коде список ul и добавить ему класс “list”.
+//2) На li через один (начиная с самого первого) установить класс “item”
+//3) На все ссылки в примере установить класс “custom-link”
+
+const ul = document.querySelector('ul');
+ul.classList.add('list');
+
+const liItems = document.querySelectorAll('li');
+for (let i = 0; i < liItems.length; i += 2) {
+  liItems[i].classList.add('item');
+}
+
+const links = document.querySelectorAll('a');
+links.forEach(function(link) {
+  link.classList.add('custom-link');
+});
