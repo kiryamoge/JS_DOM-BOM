ae8230b13952cf5b6ef5b5006073cf1681c023b7
kiryamoge <139700463+kiryamoge@users.noreply.github.com>
2023-10-01 23:54:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const galleryImages = document.querySelectorAll('.galleryimage');
 
-galleryImages.forEach((image) => {
-  image.addEventListener('click', () => {
-    const previousimage = document.querySelector('.selectedimage');
-    if (previousimage){
-      previousimage.classList.remove('selectedimage');
-    }
-    image.classList.add('selectedimage');
-    const selectedimageId = image.getAttribute('imgId');
-    localStorage.setItem('selectedimageId');
-  });
-});
+const wrapper = document.querySelector('.images');
+const galleryImages = Array.from(document.querySelectorAll('.galleryimage'));
 
+if(localStorage.activeSrc){
+  document.body.style.backgroundImage = `url(${localStorage.activeSrc})`;
+} else{
+  document.body.style.backgroundImage = `url(${galleryImages[0].src})`;
+}
+
+wrapper.addEventListener('click', (e) => {
+  if(e.target.matches('img')){
+    const activeImage = e.target;
+    galleryImages.forEach(img => img.classList.remove('active'));
+    activeImage.classList.add('active');
+    const activeSrc = activeImage.getAttribute('src');
+    localStorage.activeSrc = activeSrc;
+    document.body.style.backgroundImage = `url(${activeSrc})`
+  }
+})
